e5936c9ef220d75ffc5fcfe0249d4359b85e8ba6
yhegdesu <36450788+yhegdesu@users.noreply.github.com>
2018-12-22 07:26:19 -0500

    Saving clean.R

--- clean.R
+++ clean.R
@@ -5,6 +5,7 @@ str(raw_data)
 #replace missing ozone value with the mean
 tmp_data <- raw_data
 m <- mean(tmp_data$Ozone)
+tmp_data$Ozone[is.na(tmp_data$Ozone] <- m
 
 #remove other NAs
 clean_data <- na.omit(raw_data)
