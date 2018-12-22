cea7d987b85fdaa78cc8a876756ef493c4feec6d
yhegdesu <36450788+yhegdesu@users.noreply.github.com>
2018-12-22 07:31:47 -0500

    Saving clean.R

--- clean.R
+++ clean.R
@@ -5,7 +5,9 @@ str(raw_data)
 #define output dataset, initially just the input dataset
 clean_data <- tmp_data
 
+#
 #replace any missing ozone values with the mean of ozone
+#   First, get the mean of all valid ozone values
 m <- mean(clean_data$Ozone, na.rm=TRUE)
 m
 clean_data$Ozone[is.na(clean_data$Ozone)] <- m
